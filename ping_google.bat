9435d4880e4747fc9646fed5bcbcb51694656b8c
Denis <62012524+asathor616@users.noreply.github.com>
2022-09-27 11:48:23 +0200

    Update ping_google.bat

--- ping_google.bat
+++ ping_google.bat
@@ -6,6 +6,6 @@ REM this line show you the ping results in display
 ping www.google.com -n 1 
 
 REM this line print the ping results in the log file
-ping www.google.com -n 10 >>PingTest.log
+ping www.google.com -n 10 >>PingGoogle.log
 sleep -m 1000
 GOTO LOOPSTART
